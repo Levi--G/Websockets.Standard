32f3c6a6a7934a12af48cad3a6b337200b417fe5
Levi Gillis <gillislevi@gmail.com>
2018-01-22 10:59:02 +0100

    Added Websocket4Net test/example

---
+++ Websockets.Websocket4NetTests/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Websockets.SharedTest;
+
+namespace Websockets.Websocket4NetTests
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // 1) Link in your main activity
+            Websockets.WP8.WebsocketConnection.Link();
+
+            Console.WriteLine("Press ENTER to stop");
+
+            var test = new Test();
+            test.OnOutput += (s, data) => { Console.WriteLine(data); };
+            test.DoTest(false);
+
+            Console.ReadLine();
+
+            test.EndTest();
+        }
+    }
+}
